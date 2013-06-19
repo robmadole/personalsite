c286c989c4b8f073fbf4a3bb2e7c14d98c496772
robmadole <robmadole@gmail.com>
2013-06-18 22:12:19 -0500

    Rename article template

--- personalsite/routes.py
+++ personalsite/routes.py
@@ -4,34 +4,34 @@ from personalsite.web import app
 from personalsite.parser.article import loader
 from personalsite.presenters import ArticlePresenter, NotFound
 
-articles = loader.find(app.config.get('ARTICLES_DIRECTORY'))
+articles = list(loader.find(app.config.get('ARTICLES_DIRECTORY')))
 
 
 @app.route('/')
 def root():
     return render_template(
-        'article.html',
+        'article_detail.html',
         article=ArticlePresenter(articles).latest())
 
 
 @app.route('/articles')
-def articles_list():
+def article_list():
     try:
         return render_template(
             'article_list.html',
             articles=ArticlePresenter(articles))
     except NotFound:
-        abort()
+        abort(404)
 
 
 @app.route('/articles/<slug>')
 def article_detail(slug):
     try:
         return render_template(
-            'article.html',
+            'article_detail.html',
             article=ArticlePresenter(articles).by_slug(slug))
     except NotFound:
-        abort()
+        abort(404)
 
 
 @app.template_filter('article_date')
