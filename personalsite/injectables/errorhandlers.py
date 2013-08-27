bc0b056230e0671b115966a00751d2469cf0239f
robmadole <robmadole@gmail.com>
2013-08-27 18:51:16 -0500

    Move error templates

--- personalsite/injectables/errorhandlers.py
+++ personalsite/injectables/errorhandlers.py
@@ -4,4 +4,4 @@ from flask import render_template
 def inject(app):
     @app.errorhandler(404)
     def page_not_found(e):
-        return render_template('404.html', exception=e), 404
+        return render_template('error/404.html', exception=e), 404
